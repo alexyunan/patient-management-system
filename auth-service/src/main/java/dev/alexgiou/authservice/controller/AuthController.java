4698d46deb19cc23ea058f4eb63727d7272ddd3f
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2025-06-04 20:51:41 +0300

    feat : token validation

--- auth-service/src/main/java/dev/alexgiou/authservice/controller/AuthController.java
+++ auth-service/src/main/java/dev/alexgiou/authservice/controller/AuthController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -35,5 +32,17 @@ public class AuthController {
         }
         String token = tokenOptional.get();
         return ResponseEntity.ok(new LoginResponse(token));
+    }
+
+    @Operation(summary = "Validate token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader) {
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return authService.validateToken(authHeader.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
--- auth-service/src/main/java/dev/alexgiou/authservice/service/AuthService.java
+++ auth-service/src/main/java/dev/alexgiou/authservice/service/AuthService.java
@@ -2,6 +2,7 @@ package dev.alexgiou.authservice.service;
 
 import dev.alexgiou.authservice.dto.LoginRequest;
 import dev.alexgiou.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,9 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
+    /**
+     * Authenticates a user based on the provided login request.
+     */
     public Optional<String> authenticate(LoginRequest loginRequest) {
         Optional<String> token = userService.findByEmail(loginRequest.getEmail())
                 .filter(u -> passwordEncoder.matches(loginRequest.getPassword(), u.getPassword()))
@@ -27,5 +31,14 @@ public class AuthService {
 
         return token;
 
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 }
