1a11e9323af7d2e78576011cc8f66c779d9a3b42
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2025-07-24 16:41:04 +0300

    add new endpoint for patient service -> find by patient id.

--- patient-service/src/main/java/dev/alexgiou/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/dev/alexgiou/patientservice/controller/PatientController.java
@@ -8,8 +8,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
+
 import java.util.List;
 import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -28,37 +30,44 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Patient", description = "API for patient management")
 public class PatientController {
 
-  private final PatientService patientService;
+    private final PatientService patientService;
 
-  @GetMapping
-  @Operation(summary = "Get all patients")
-  public ResponseEntity<List<PatientResponse>> getPatients() {
-    List<PatientResponse> patients = patientService.getAllPatients();
-    return ResponseEntity.ok().body(patients);
-  }
+    @GetMapping
+    @Operation(summary = "Get all patients")
+    public ResponseEntity<List<PatientResponse>> getPatients() {
+        List<PatientResponse> patients = patientService.getAllPatients();
+        return ResponseEntity.ok().body(patients);
+    }
 
-  @PostMapping
-  @Operation(summary = "Create a new patient")
-  public ResponseEntity<PatientResponse> createPatient(@Validated({Default.class,
-      CreatePatientValidationGroup.class}) @RequestBody PatientRequest patientRequest) {
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a patient by ID")
+    public ResponseEntity<PatientResponse> getPatientById(@PathVariable UUID id) {
+        PatientResponse patient = patientService.getPatientById(id);
+        return ResponseEntity.ok().body(patient);
+    }
 
-    PatientResponse patient = patientService.createPatient(patientRequest);
-    return ResponseEntity.ok().body(patient);
-  }
+    @PostMapping
+    @Operation(summary = "Create a new patient")
+    public ResponseEntity<PatientResponse> createPatient(@Validated({Default.class,
+            CreatePatientValidationGroup.class}) @RequestBody PatientRequest patientRequest) {
 
-  @PutMapping("/{id}")
-  @Operation(summary = "Update an existing patient")
-  public ResponseEntity<PatientResponse> updatePatient(@PathVariable UUID id,
-      @Validated({Default.class}) @RequestBody PatientRequest patientRequest) {
+        PatientResponse patient = patientService.createPatient(patientRequest);
+        return ResponseEntity.ok().body(patient);
+    }
 
-    PatientResponse patient = patientService.updatePatient(id, patientRequest);
-    return ResponseEntity.ok().body(patient);
-  }
+    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing patient")
+    public ResponseEntity<PatientResponse> updatePatient(@PathVariable UUID id,
+                                                         @Validated({Default.class}) @RequestBody PatientRequest patientRequest) {
 
-  @DeleteMapping("/{id}")
-  @Operation(summary = "Delete an existing patient")
-  public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
-    patientService.deletePatient(id);
-    return ResponseEntity.noContent().build();
-  }
+        PatientResponse patient = patientService.updatePatient(id, patientRequest);
+        return ResponseEntity.ok().body(patient);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an existing patient")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- patient-service/src/main/java/dev/alexgiou/patientservice/service/PatientService.java
+++ patient-service/src/main/java/dev/alexgiou/patientservice/service/PatientService.java
@@ -9,9 +9,11 @@ import dev.alexgiou.patientservice.kafka.KafkaProducer;
 import dev.alexgiou.patientservice.mapper.PatientMapper;
 import dev.alexgiou.patientservice.model.Patient;
 import dev.alexgiou.patientservice.repository.PatientRepository;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,56 +22,63 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PatientService {
 
-  private final PatientRepository patientRepository;
-  private final BillingServiceGrpcClient billingServiceGrpcClient;
-  private final KafkaProducer kafkaProducer;
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
 
-  public List<PatientResponse> getAllPatients() {
-    return patientRepository.findAll()
-        .stream()
-        .map(PatientMapper::toDto)
-        .toList();
-  }
-
-  @Transactional
-  public PatientResponse createPatient(PatientRequest patientRequest) {
-    if (patientRepository.existsByEmail(patientRequest.getEmail())) {
-      throw new EmailAlreadyExistsException("A patient with this email already exists.");
+    public List<PatientResponse> getAllPatients() {
+        return patientRepository.findAll()
+                .stream()
+                .map(PatientMapper::toDto)
+                .toList();
     }
 
-    Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequest));
-
-    billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
-
-    kafkaProducer.sendEvent(newPatient);
-
-    return PatientMapper.toDto(newPatient);
-  }
-
-  public PatientResponse updatePatient(UUID id, PatientRequest patientRequest) {
-    Patient patient = patientRepository.findById(id)
-        .orElseThrow(() -> new PatientNotFoundException("Patient not found with id: " + id));
-
-    if (patientRepository.existsByEmailAndIdNot(patientRequest.getEmail(), id)) {
-      throw new EmailAlreadyExistsException("A patient with this email already exists.");
+    //Find a patient by id
+    public PatientResponse getPatientById(UUID id) {
+        return patientRepository.findById(id)
+                .map(PatientMapper::toDto)
+                .orElseThrow(() -> new PatientNotFoundException("Patient not found with id: " + id));
     }
 
-    patient.setName(patientRequest.getName());
-    patient.setEmail(patientRequest.getEmail());
-    patient.setAddress(patientRequest.getAddress());
-    patient.setDateOfBirth(LocalDate.parse(patientRequest.getDateOfBirth()));
+    @Transactional
+    public PatientResponse createPatient(PatientRequest patientRequest) {
+        if (patientRepository.existsByEmail(patientRequest.getEmail())) {
+            throw new EmailAlreadyExistsException("A patient with this email already exists.");
+        }
 
-    Patient updatedPatient = patientRepository.save(patient);
-    return PatientMapper.toDto(updatedPatient);
-  }
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequest));
 
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
 
-  public void deletePatient(UUID id) {
-    if (!patientRepository.existsById(id)) {
-      throw new PatientNotFoundException("Patient not found with id: " + id);
+        kafkaProducer.sendEvent(newPatient);
+
+        return PatientMapper.toDto(newPatient);
     }
-    patientRepository.deleteById(id);
-  }
+
+    public PatientResponse updatePatient(UUID id, PatientRequest patientRequest) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new PatientNotFoundException("Patient not found with id: " + id));
+
+        if (patientRepository.existsByEmailAndIdNot(patientRequest.getEmail(), id)) {
+            throw new EmailAlreadyExistsException("A patient with this email already exists.");
+        }
+
+        patient.setName(patientRequest.getName());
+        patient.setEmail(patientRequest.getEmail());
+        patient.setAddress(patientRequest.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequest.getDateOfBirth()));
+
+        Patient updatedPatient = patientRepository.save(patient);
+        return PatientMapper.toDto(updatedPatient);
+    }
+
+
+    public void deletePatient(UUID id) {
+        if (!patientRepository.existsById(id)) {
+            throw new PatientNotFoundException("Patient not found with id: " + id);
+        }
+        patientRepository.deleteById(id);
+    }
 
 
 }
