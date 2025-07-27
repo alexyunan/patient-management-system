eb85d02d4e67583a24f1cb60f9bb10a056c85e6b
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2025-07-27 20:19:35 +0300

    add ecs cluster

--- infrastructure/src/main/java/dev/alexgiou/stack/LocalStack.java
+++ infrastructure/src/main/java/dev/alexgiou/stack/LocalStack.java
@@ -3,6 +3,8 @@ package dev.alexgiou.stack;
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ecs.CloudMapNamespaceOptions;
+import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
+    private final Cluster ecsCluster;
 
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
@@ -29,6 +32,8 @@ public class LocalStack extends Stack {
         CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDbHealthCheck");
 
         CfnCluster mskCluster = createMskCluster();
+
+        this.ecsCluster = createEcsCluster();
     }
 
     private Vpc createVpc() {
@@ -80,6 +85,14 @@ public class LocalStack extends Stack {
                 .build();
     }
 
+    private Cluster createEcsCluster() {
+        return Cluster.Builder.create(this, "PatientManagementEcsCluster")
+                .vpc(vpc)
+                .defaultCloudMapNamespace(CloudMapNamespaceOptions.builder()
+                        .name("patient-management.local")
+                        .build())
+                .build();
+    }
 
     public static void main(final String[] args) {
         App app = new App(AppProps.builder().outdir("./cdk.out").build());
