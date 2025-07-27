069304a9442cd8061cf52024a0a466b76f1d0459
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2025-07-27 20:07:30 +0300

    add kafka cluster

--- infrastructure/src/main/java/dev/alexgiou/stack/LocalStack.java
+++ infrastructure/src/main/java/dev/alexgiou/stack/LocalStack.java
@@ -1,14 +1,14 @@
 package dev.alexgiou.stack;
 
 import software.amazon.awscdk.*;
-import software.amazon.awscdk.services.ec2.InstanceClass;
-import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 import javax.xml.crypto.Data;
+import java.util.stream.Collectors;
 
 /**
  * @author: Alexandros Giounan
@@ -27,6 +27,8 @@ public class LocalStack extends Stack {
 
         CfnHealthCheck audthDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDbHealthCheck");
         CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDbHealthCheck");
+
+        CfnCluster mskCluster = createMskCluster();
     }
 
     private Vpc createVpc() {
@@ -62,6 +64,22 @@ public class LocalStack extends Stack {
                 .build();
 
     }
+
+    private CfnCluster createMskCluster(){
+        return CfnCluster.Builder.create(this, "MskCluster")
+                .clusterName("kafka-cluster")
+                .kafkaVersion("2.8.0")
+                .numberOfBrokerNodes(1)
+                .brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+                        .instanceType("kafka.m5.xlarge")
+                                .clientSubnets(vpc.getPrivateSubnets().stream()
+                                        .map(ISubnet::getSubnetId)
+                                        .collect(Collectors.toList()))
+                                .brokerAzDistribution("DEFAULT")
+                                .build())
+                .build();
+    }
+
 
     public static void main(final String[] args) {
         App app = new App(AppProps.builder().outdir("./cdk.out").build());
